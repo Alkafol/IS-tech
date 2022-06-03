a7e8a1a5959ba4ca3788af82f6f3a4361c024632
Alkafol <semenovroman8@gmail.com>
2022-06-03 16:43:39 +0300

    fixed: bug

--- kotiki-java/src/test/java/com/techprog/upgradedcats/UpgradedcatsApplicationTests.java
+++ kotiki-java/src/test/java/com/techprog/upgradedcats/UpgradedcatsApplicationTests.java
@@ -3,10 +3,13 @@ package com.techprog.upgradedcats;
 import com.techprog.upgradedcats.dao.CatRepository;
 import com.techprog.upgradedcats.dao.OwnerRepository;
 import com.techprog.upgradedcats.dao.UserRepository;
+import com.techprog.upgradedcats.models.Owner;
+import com.techprog.upgradedcats.models.User;
 import com.techprog.upgradedcats.repository.CatDto;
 import com.techprog.upgradedcats.models.Cat;
 import com.techprog.upgradedcats.models.Color;
 import com.techprog.upgradedcats.service.CatsService;
+import com.techprog.upgradedcats.tools.CatOwnershipException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,13 +18,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Calendar;
 import java.util.Optional;
 
+import static com.techprog.upgradedcats.security.ApplicationUserRole.ADMIN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class UpgradedcatsApplicationTests {
     
     @Test
-    public void additionTest() {
+    public void additionTest() throws CatOwnershipException {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.YEAR, 2015);
         cal.set(Calendar.MONTH, Calendar.JULY);
@@ -33,15 +37,18 @@ class UpgradedcatsApplicationTests {
         PasswordEncoder passwordEncoder = Mockito.mock(PasswordEncoder.class);
         CatsService catsService = new CatsService(catRepository, ownerRepository, userRepository, passwordEncoder);
 
-        Cat cat = new Cat("Barsik", cal, "Scottish fold", Color.grey, null);
+        User user = new User("anton", "1234", ADMIN);
+        Owner owner = new Owner("Anton", cal, user);
+        Cat cat = new Cat("Barsik", cal, "Scottish fold", Color.grey, owner);
         Optional<Cat> optionalCat = Optional.of(cat);
         String id = cat.getId();
         Mockito.when(catRepository.findById(id)).thenReturn(optionalCat);
+        Mockito.when(userRepository.findByUsername(user.getUsername())).thenReturn(user);
 
         CatDto catDto = catsService.convertToCatDto(cat);
 
-        // CatDto foundCat = catsService.getCatById(id);
+        CatDto foundCat = catsService.getCatById(id, user.getUsername());
 
-        // assertEquals(catDto.getId(), foundCat.getId());
+        assertEquals(catDto.getId(), foundCat.getId());
     }
 }
