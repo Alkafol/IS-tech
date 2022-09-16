35f173a4d1318246ddeaca16a2adf2c0039375aa
Alkafol <76702841+Alkafol@users.noreply.github.com>
2022-09-16 18:01:47 +0300

    Delete kotiki-java/src/main/java/com/techprog/upgradedcats/dao directory

--- kotiki-java/src/main/java/com/techprog/upgradedcats/dao/CatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techprog.upgradedcats.dao;
-
-import com.techprog.upgradedcats.models.Cat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CatRepository extends JpaRepository<Cat, String> { }
