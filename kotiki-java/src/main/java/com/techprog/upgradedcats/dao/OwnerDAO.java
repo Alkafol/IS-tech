65aa6be8c781022851255894584ddf8f29df232a
Alkafol <semenovroman8@gmail.com>
2022-10-22 23:36:47 +0300

    refactor: remove unnecessary files

--- kotiki-java/src/main/java/com/techprog/upgradedcats/dao/OwnerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.techprog.upgradedcats.dao;
-
-import com.techprog.upgradedcats.models.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerDAO extends JpaRepository<Owner, String>{}
-
